c5bd1676b42773a21f3744a532f278a640774793
Wano-k <mmarie.laporte@gmail.com>
2015-12-28 16:57:35 +0100

    Ajout de free_entry

--- ll.h
+++ ll.h
@@ -15,5 +15,6 @@ error stop_disk(disk_id id);
 
 error free_block(disk_id* id, uint32_t num_partition, uint32_t num_free);
 error fill_block(disk_id* id, uint32_t num_partition, uint32_t num_fill);
+error free_entry(disk_id* id, uint32_t num_partition, uint32_t entry_index);
 error put_dir_entry(disk_id* id, uint32_t num_partition, TTTFS_File_Table_Entry entry);
 #endif
--- tfs_manipulation.c
+++ tfs_manipulation.c
@@ -84,6 +84,40 @@ error fill_block(disk_id* id, uint32_t num_partition, uint32_t num_fill){
   return e;
 }
 
+// mettre une entrée de répertoire dans la liste des entrées libres = rendre une entrée libre
+error free_entry(disk_id* id, uint32_t num_partition, uint32_t entry_index){
+  error e;
+  e.val=0;
+  block *partition_block = malloc(sizeof(block));
+  block *file_table_block = malloc(sizeof(block));
+  int first_free_block;
+  int offset = (int)(FILE_TABLE_BLOCK_SIZE*sizeof(uint32_t));
+  uint32_t temp;
+  int a;
+  int num_block_entry = num_partition+1+(entry_index / offset);
+
+  // Récupération des blocks
+  read_block(id,partition_block,num_partition);
+  read_block(id,file_table_block,num_block_entry);
+
+  // Récupration du premier block libre
+  memcpy(&temp,(partition_block->octets) + (4*sizeof(uint32_t)),sizeof(uint32_t));
+  first_free_block = uitoi(temp);
+
+  // Modification du suivant de la nouvelle entrée libre + premier file libre de partition
+  a = itoui(first_free_block);
+  memcpy((file_table_block->octets) + ((entry_index % offset)*FILE_TABLE_BLOCK_SIZE) - sizeof(uint32_t),&a,sizeof(uint32_t));
+  a = itoui(entry_index);
+  memcpy((partition_block->octets) + (4*sizeof(uint32_t)),&a,sizeof(uint32_t));
+
+  // Réécriture des blocks
+  write_block(id,partition_block,num_partition);
+  write_block(id,file_table_block,num_block_entry);
+  sync_disk(id);
+
+  return e;
+}
+
 error put_dir_entry(disk_id* id, uint32_t num_partition, TTTFS_File_Table_Entry entry){
   error e;
   e.val=0;

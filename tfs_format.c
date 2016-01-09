db2e9eb38d43d18fc34783c79d12a2a1c72780a7
Wano-k <mmarie.laporte@gmail.com>
2016-01-09 15:59:01 +0100

    Avancée de tfs_format

--- tfs_format.c
+++ tfs_format.c
@@ -16,11 +16,12 @@ int main(int argc, char* argv[]){
     uint32_t temp;
     int a,i,nb,npart,mf,first,pemplacement=0;
     char* name;
-    
     disk_id* id;
     error e;
     block *block0;
-    
+    block *block_file_table;
+    TTTFS_File_Table_Entry entry_root;
+
     if (argc < 5){
         fprintf(stderr, "Error : wrong number of arguments.\n");
         return -1;
@@ -92,53 +93,95 @@ int main(int argc, char* argv[]){
         printf("changing file_count to %i\n",mf);
     }
 
-        //printf("cas n : \n\t mf/ftb:%i \n\t mf:%i \n\t first:%i \n\t a:%i\n",mf/FILE_TABLE_BLOCK_SIZE+1,mf,first,a);
-        read_block(id,block0,pemplacement);
-        //first = 2+(mf/FILE_TABLE_BLOCK_SIZE);
-        a-=first;
-        //printf("a : %i\n",a);
+    //printf("cas n : \n\t mf/ftb:%i \n\t mf:%i \n\t first:%i \n\t a:%i\n",mf/FILE_TABLE_BLOCK_SIZE+1,mf,first,a);
+    read_block(id,block0,pemplacement);
+    //first = 2+(mf/FILE_TABLE_BLOCK_SIZE);
+    a-=first;
+    //printf("a : %i\n",a);
         
-        temp = itoui(a);//nombre de blocks libres
-        memcpy((block0->octets) + (3*sizeof(uint32_t)),&temp,sizeof(uint32_t));
-        temp = itoui(first); // premier block libre
-        memcpy((block0->octets) + (4*sizeof(uint32_t)),&temp,sizeof(uint32_t));
-        // le nombre de fichiers supportables
-        if (a<mf) {// verification du nombre de fichier max
-            temp = itoui(a);
+    temp = itoui(a);//nombre de blocks libres
+    memcpy((block0->octets) + (3*sizeof(uint32_t)),&temp,sizeof(uint32_t));
+    temp = itoui(first); // premier block libre
+    memcpy((block0->octets) + (4*sizeof(uint32_t)),&temp,sizeof(uint32_t));
+    // le nombre de fichiers supportables
+    if (a<mf) {// verification du nombre de fichier max
+      temp = itoui(a);
             
-        }
-        else{
-            temp = itoui(mf);
-            //printf("temp: %i\n",temp);
-        }
-        memcpy((block0->octets) + (5*sizeof(uint32_t)),&temp,sizeof(uint32_t));
-        // le nombre de fichiers actuellement libres
-        if (a<mf) {// verification du nombre de fichier max
-            temp = itoui(a);
-        }
-        else{
-            temp = itoui(mf);
-        }
-        memcpy((block0->octets) + (6*sizeof(uint32_t)),&temp,sizeof(uint32_t));
-        temp = itoui(first); // le numero du premier fichier libre du volume
-        memcpy((block0->octets) + (7*sizeof(uint32_t)),&temp,sizeof(uint32_t));
-        // Next free file
+    }
+    else{
+      temp = itoui(mf);
+      //printf("temp: %i\n",temp);
+    }
+    memcpy((block0->octets) + (5*sizeof(uint32_t)),&temp,sizeof(uint32_t));
+    // le nombre de fichiers actuellement libres
+    if (a<mf) {// verification du nombre de fichier max
+      temp = itoui(a);
+    }
+    else{
+      temp = itoui(mf);
+    }
+    memcpy((block0->octets) + (6*sizeof(uint32_t)),&temp,sizeof(uint32_t));
+    temp = itoui(first); // le numero du premier fichier libre du volume
+    memcpy((block0->octets) + (7*sizeof(uint32_t)),&temp,sizeof(uint32_t));
+    // Next free file
     
     int j;
     for (j = first; j < a; j++){
-        if (j == a-1){
-            a = itoui(j);
-        }
-        else{
-            a = itoui(j+1);
-        }
-        block *partition_sub_block = malloc(sizeof(block));
-        read_block(id,partition_sub_block,j);
-        memcpy((partition_sub_block->octets) + (TTTFS_VOLUME_BLOCK_SIZE-sizeof(uint32_t)),&a,sizeof(uint32_t));
-        write_block(id,partition_sub_block,j);
+      if (j == a-1){
+	a = itoui(j);
+      }
+      else{
+	a = itoui(j+1);
+      }
+      block *partition_sub_block = malloc(sizeof(block));
+      read_block(id,partition_sub_block,j);
+      memcpy((partition_sub_block->octets) + (TTTFS_VOLUME_BLOCK_SIZE-sizeof(uint32_t)),&a,sizeof(uint32_t));
+      write_block(id,partition_sub_block,j);
     }
-
     write_block(id,block0,pemplacement);
+
+
+
+    // ---------------- Construction du répertoire racine
+
+    // On récuppère le numéro de bloc qui sera occupé par la racine
+    read_block(id,block0,pemplacement);
+    memcpy(&temp,block0->octets,sizeof(uint32_t));
+    first = uitoi(temp);
+    
+    // Création de l'entrée
+    block_file_table = malloc(sizeof(block));
+    read_block(id,block_file_table,pemplacement+1);
+    entry_root.size = sizeof(uint32_t)+28;
+    entry_root.type = 0;
+    entry_root.sub_type = 1;
+    entry_root.tfs_direct[0] = first;
+    for (i = 1; i < 10; i++){
+      entry_root.tfs_direct[i] = 0;
+    }
+    entry_root.tfs_indirect1 = 0;
+    entry_root.tfs_indirect2 = 0;
+
+    // Remplissage
+    if (fill_block(id,npart).val != 0){
+      fprintf(stderr, "Error while filling block.\n");
+      return -1;
+    }
+    if (fill_entry(id,npart,entry_root).val != 0){
+      fprintf(stderr, "Error while writing on File Table.\n");
+      return -1;
+    }
+    write_block(id,block_file_table,pemplacement+1);
+    char* buf_d = malloc(sizeof(uint32_t)+28);
+    read_block(id,block0,first);
+    temp = itoui(0);
+    memcpy(buf_d,&temp,sizeof(uint32_t));
+    strncpy(&buf_d[sizeof(uint32_t)],".\0",28);
+    memcpy((block0->octets),buf_d,sizeof(uint32_t)+28);
+    strncpy(&buf_d[sizeof(uint32_t)],"..\0",28);
+    memcpy((block0->octets)+(sizeof(uint32_t)+28),buf_d,sizeof(uint32_t)+28);
+
+
     sync_disk(id);
     printf("Partition %i formated !\n",npart);
     return 0;

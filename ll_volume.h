02f7dd86f62f54846d00e9a69fd20b7fd1663246
vivena <Wintermute@macbook-pro-de-guyot.home>
2016-01-10 16:28:02 +0100

    mise à jour du header

--- ll_volume.h
+++ ll_volume.h
@@ -7,3 +7,4 @@ error free_entry(disk_id* id, uint32_t num_partition, uint32_t entry_index);
 error fill_entry(disk_id* id, uint32_t num_partition, TTTFS_File_Table_Entry* entry);
 int cut_pathname(char** res, const char* path);
 int is_in_directory(disk_id* id, int current_dir, int pemplacement, char* name);
+int have_next_block(disk_id* id, uint32_t num_partition,int entry_index,int num_block);

1ccb67155c89f62509742e3d6df646eaef0fd94a
Wano-k <mmarie.laporte@gmail.com>
2016-01-10 15:46:05 +0100

    tfs_open à jour

--- tfs_open.c
+++ tfs_open.c
@@ -12,12 +12,11 @@
 
 #include <fcntl.h>
 
-int tfs_open(char *path,int  access,  int  permission ){
+int tfs_open(char *path,int access, int permission ){
     char** splitPath;
     char** directories;
     char* disk_name, current_dir;
-    //char dir_name[TFS_DIRECTORIES_SIZE];
-    int i,temp, type, partition, pemplacement, first_free_block, first_free_file, exists, index, index_entry, file_size,fd,rez=-1;
+    int a, i,temp, type, partition, pemplacement, exists, index, index_entry, fd, inode, append, rez=-1;
     
     error e;
     
@@ -26,7 +25,6 @@ int tfs_open(char *path,int  access,  int  permission ){
     block* block_entry = malloc(sizeof(block));
     block* block_navigation = malloc(sizeof(block));
     disk_id* id = malloc(sizeof(disk_id));
-    //TTTFS_File_Table_Entry* entry_dir = malloc(sizeof(TTTFS_File_Table_Entry));
     
     // Découpage du path
     splitPath = malloc(sizeof(char*));
@@ -67,9 +65,80 @@ int tfs_open(char *path,int  access,  int  permission ){
             pemplacement++;
             read_block(id,block_partition,pemplacement);
             
-            
-            
-            
+            // Pour chaque répertoire
+	    current_dir = 0; // numéro de l'entrée dans le File Table
+	    while (directories[0]){
+
+	      // ----------- Test de l'existence de ce répertoire
+	      exists = is_in_directory(id,current_dir,pemplacement,directories[0]);
+
+	      // ----------- Action en fonction de l'existence
+
+	      // Si on est au dernier repertoire
+	      if (!directories[1][0]){
+		printf("Preparing to create %s...\n", directories[0]);
+		if (!exists){ // Si n'existe pas, on créé un nouveau fichier
+
+		  break;
+		}
+		else{ // Sinon, erreur
+		  printf("Opening %s...\n", directories[0]);
+
+		  // On récuppère le numéro de bloc qui sera occupé par le fd
+		  memcpy(&temp,(block_partition->octets)+(4*sizeof(uint32_t)),sizeof(uint32_t));
+		  fd = uitoi(temp);
+		  //memcpy(&temp,(block_partition->octets)+(7*sizeof(uint32_t)),sizeof(uint32_t));
+		  //fd = uitoi(temp);
+
+		  // Lecture du File Table (recherche du répertoire courrant)
+		  index = pemplacement+1+(current_dir/FILE_TABLE_OFFSET);
+		  read_block(id,block_entry,index);
+		  index_entry = (current_dir % FILE_TABLE_OFFSET)*FILE_TABLE_BLOCK_SIZE;
+
+		  // Récupération de l'inode du fichier
+		  memcpy(&temp,(block_entry->octets) + index_entry + ((3+i)*sizeof(uint32_t)),sizeof(uint32_t));
+		  inode = uitoi(temp);
+
+		  // Ecriture du fd dans le File Table
+		  a = itoui(fd);
+		  memcpy((block_entry->octets) + index_entry + ((3+i)*sizeof(uint32_t)),&a,sizeof(uint32_t));
+		  write_block(id,block_entry,index);
+
+		  // Remplissage du fd
+		  if (fill_block(id,pemplacement).val != 0){
+		    fprintf(stderr, "Error while filling block.\n");
+		    return -1;
+		  }
+		  read_block(id,block_navigation,fd);
+		  temp = itoui(inode);
+		  memcpy((block_navigation->octets),&temp,sizeof(uint32_t));
+		  append = 0;
+		  temp = itoui(append);
+		  memcpy((block_navigation->octets) + sizeof(uint32_t),&temp,sizeof(uint32_t));
+		  temp = itoui(access);
+		  memcpy((block_navigation->octets) + (2*sizeof(uint32_t)),&temp,sizeof(uint32_t));
+		  temp = itoui(permission);
+		  memcpy((block_navigation->octets) + (3*sizeof(uint32_t)),&temp,sizeof(uint32_t));
+		  write_block(id,block_navigation,fd);
+		  sync_disk(id);
+
+		  return fd;
+		}
+	      }
+	      // Si on n'est pas encore au dernier repertoire
+	      else{
+		printf("Searching for %s... ", directories[0]);
+	
+		if (!exists){
+		  printf("\nError : directory %s doesn't exists.\n", directories[0]);
+		  return -1;
+		}
+		else{
+		  printf("ok\n");
+		}
+	      }
+	      directories++;
+	    }
             break;
         default:
             rez=-1;

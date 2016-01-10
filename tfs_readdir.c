4a85ecbddbdee32e182b60929b9fc7a4935470cd
vivena <Wintermute@macbook-pro-de-guyot.home>
2016-01-10 17:07:06 +0100

    readdir

--- tfs_readdir.c
+++ tfs_readdir.c
@@ -5,5 +5,32 @@
 //  Created by Wintermute on 10/01/2016.
 //
 //
+#include <sys/types.h>
+#include <dirent.h>
 
-#include <stdio.h>
+#include "main.h"
+#include "ll.h"
+#include "ll_volume.h"
+
+
+#define DIRENT_SIZE 32
+
+struct dirent *tfs_readdir(DIR *dirp,disk_id* id){
+    uint32_t temp;
+    char buf[DIRENT_SIZE];
+    struct dirent *rez;
+    
+    rez=malloc(sizeof(struct dirent *));
+    if (id==NULL) {
+        return readdir(dirp);
+    }
+    else{
+        memcpy(buf,dirp+(dirp->__dd_loc),32*sizeof(char));
+        dirp->__dd_loc+=32;
+        memcpy(&temp,buf,sizeof(uint32_t));
+        rez->d_ino=uitoi(temp);
+        memcpy(rez->d_name,buf+sizeof(uint32_t),32-sizeof(uint32_t));
+        
+    }
+    return rez;
+}
